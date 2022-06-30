6ddb0a456ebb4a156cfa12cb1a590f3cf8ac8367
vincentramirez <vinnie.ramirez@gmail.com>
2022-06-29 20:31:49 -0700

    demo code

--- outputs.tf
+++ outputs.tf
@@ -3,13 +3,13 @@ output "cluster_id" {
 }
 
 output "cluster_id" {
-  value = spectrocloud_cluster_aws2.cluster.id
+  value = spectrocloud_cluster_aws.cluster2.id
 }
 
 output "cluster_kubeconfig" {
   value = local.cluster_kubeconfig
 }
 
-output "cluster_kubeconfig" {
+output "cluster_kubeconfig2" {
   value = local.cluster_kubeconfig2
 }
